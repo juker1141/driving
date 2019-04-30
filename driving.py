d8dadba89c8a91920dfb855e4c3568b9096f2960
Juker1141 <juker1141@gmail.com>
2019-05-01 03:28:36 +0800

    Update US part

--- driving.py
+++ driving.py
@@ -1,9 +1,13 @@
 country = input('請輸入您的國家: ')
 age = input('請輸入您的年齡: ')
 age = int(age)
-
 if country == '台灣':
-    if age >= 18 :
-        print('您可以考駕照')
-    else :
-    	print('您還不能考駕照')
+	if age >= 18:
+		print('您可以考取駕照')
+	else:
+		print('您還不能考駕照')
+elif country == '美國':
+	if age >= 16:
+		print('您可以考取駕照')
+	else:
+		print('您還不能考駕照')
